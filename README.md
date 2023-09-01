7322be87e671b1104bf7c9e4a40f88e49a948093
Evan Almloff <evanalmloff@gmail.com>
2023-09-01 10:45:23 -0500

    add documentation

---
+++ README.md
@@ -0,0 +1,13 @@
+Run with:
+```sh
+cargo run
+```
+
+This should succeed. 
+
+However once you run cargo update it will fail:
+
+```sh
+cargo update
+cargo run
+```
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 fn main() {
     tauri_bundler::DebianSettings {
         depends: None,
-         files:HashMap::new(),
+        files:HashMap::new(),
+        // More fields are required in the 1.3.0 version of tauri-bundler
     };
 }
